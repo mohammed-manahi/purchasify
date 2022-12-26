a051ed0be1b97fd8f7b45ad74f52523753ac9058
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-26 14:54:07 +0300

    Create order and order item models and register models in admin site

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from order.models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    """
+    Include order item in order site admin as an inline display
+    """
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Register order model in admin site
+    """
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'city', 'post_code', 'created_at',
+                    'updated_at']
+    list_filter = ['created_at', 'updated_at', 'paid']
+    inlines = [OrderItemInline]
--- order/models.py
+++ order/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from shop.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    """
+    Create order model
+    """
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=250)
+    post_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [models.Index(fields=['created_at']), ]
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    """
+    Create order item model and associate it with order model and product model
+    """
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
