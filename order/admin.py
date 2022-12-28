a64dfbc8b6fc83430d6468a8dd6ae0372a56ad26
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-28 13:44:09 +0300

    Implement custom action for order model in admin site to generate csv file of orders

--- order/admin.py
+++ order/admin.py
@@ -1,5 +1,43 @@
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
 from order.models import Order, OrderItem
+
+
+def export_to_csv(modeladmin, request, queryset):
+    """
+    Create custom action in admin site for order model to generate csv file of orders
+    :param modeladmin:
+    :param request:
+    :param queryset:
+    :return orders csv file:
+    """
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    # Declare the content type of the response as csv file
+    response = HttpResponse(content_type='text/csv')
+    # Indicate that the response includes attached file
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    # Use get_fields() to get model fields and exclude any relationship with other models
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write the header row
+    writer.writerow([field.verbose_name for field in fields])
+    # Write the actual data
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+# Define the action name using the short description attribute
+export_to_csv.short_description = 'Export to CSV'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -19,3 +57,5 @@ class OrderAdmin(admin.ModelAdmin):
                     'updated_at']
     list_filter = ['created_at', 'updated_at', 'paid']
     inlines = [OrderItemInline]
+    # Add the custom action to generate csv file here
+    actions = [export_to_csv]
