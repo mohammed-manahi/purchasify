b3d35d9c126ef14ec54ff62718af34a8feea8b06
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-12-31 16:44:55 +0300

    Implement post code validation using localflavor

--- order/forms.py
+++ order/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from order.models import Order
+from localflavor.us.forms import USZipCodeField
+
 
 class OrderCreateForm(forms.ModelForm):
     """
     Create a form for creating orders
     """
+    post_code = USZipCodeField()
+
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'post_code', 'city']
